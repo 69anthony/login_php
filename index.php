2c2ff468bfe3bc973df4cc313ce7d9e7b7dd7ddb
69anthony <anthonyvalverde36@gmail.com>
2023-07-02 22:20:08 -0500

    Se agrega mensaje en main

--- index.php
+++ index.php
@@ -34,6 +34,9 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
+    <style type=css>
+    
+    </style>
 </head>
 
 <body>
